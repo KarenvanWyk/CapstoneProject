3fa92e9971901ce27794b3cda0751429534699c1
KarenvanWyk <karenvw@pepkorit.com>
2024-05-21 15:36:53 +0200

    SIKULI WORKING :-)

--- PageCart.cs
+++ PageCart.cs
@@ -8,7 +8,7 @@ namespace Capstone
         public static void AddtoCart(IWebDriver driver, ISikuliSession session) 
             
         {
-            string path = "C:\\Training\\CapstoneProject\\Images";     // Click on the product labled Create Your Own Jewelry image (Use Sikuli).  Product Page will be displayed
+            string path = "C:\\Training\\CapstoneProjectClone\\Images\\Create_Your_Own.png";     // Click on the product labled Create Your Own Jewelry image (Use Sikuli).  Product Page will be displayed
             IPattern iPattern = Patterns.FromFile(path);
             session.Wait(iPattern);
             session.Click(iPattern);
