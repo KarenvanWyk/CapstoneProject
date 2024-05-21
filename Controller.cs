87b7ba2c6b2d789fac245c8286d7dfd7c1805e68
KarenvanWyk <karenvw@pepkorit.com>
2024-05-21 14:54:51 +0200

    Finished 50% of test cases, issue with Sikuli

--- Controller.cs
+++ Controller.cs
@@ -1,16 +1,20 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SikuliSharp;
 
 namespace Capstone
 {
     public class Controller
     {
+        ISikuliSession session;
         IWebDriver driver;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
+            session = Sikuli.CreateSession();
             driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
@@ -29,19 +33,53 @@ namespace Capstone
         [Test]
         public void SelectCategory()
         {
-            
+            PageHome.OpenHomePage(driver);
+            PageLogin.Login(driver);
+
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ul/li[6]/a")).Click();  // Select the Jewelry option under the Categories section.  Jewelry downloads page is displayed
+
+            List<string> urlList = new List<string>();                                                                // Verify that the url contains Jewelry
+            urlList.Add("https://demowebshop.tricentis.com/jewelry");
+            bool isContains = urlList.Any(x => x.Contains("Jewelry"));
         }
 
         [Test]
         public void ChangeDisplayView()
         {
-            
+            PageHome.OpenHomePage(driver);
+            PageLogin.Login(driver);
+
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ul/li[6]/a")).Click();  // Select the Jewelry option under the Categories section.  Jewelry downloads page is displayed
+
+            List<string> urlList = new List<string>();                                                                // Verify that the url contains Jewelry
+            urlList.Add("https://demowebshop.tricentis.com/jewelry");
+            bool isContains = urlList.Any(x => x.Contains("Jewelry"));
+
+            driver.FindElement(By.XPath("//*[@id=\"products-viewmode\"]")).Click();                                   // Select the List option under the View as dropdown
+            Thread.Sleep(500);
+
+            driver.FindElement(By.XPath("//*[@id=\"products-viewmode\"]/option[2]")).Click();                         // Products will be displayed underneath each other
         }
 
         [Test]
         public void AddProduct()
         {
+            PageHome.OpenHomePage(driver);
+            PageLogin.Login(driver);
+
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ul/li[6]/a")).Click();  // Select the Jewelry option under the Categories section.  Jewelry downloads page is displayed
+
+            List<string> urlList = new List<string>();                                                                // Verify that the url contains Jewelry
+            urlList.Add("https://demowebshop.tricentis.com/jewelry");
+            bool isContains = urlList.Any(x => x.Contains("Jewelry"));
             
+            driver.FindElement(By.XPath("//*[@id=\"products-viewmode\"]")).Click();                                   // Select the List option under the View as dropdown
+            Thread.Sleep(500);
+
+            driver.FindElement(By.XPath("//*[@id=\"products-viewmode\"]/option[2]")).Click();                         // Products will be displayed underneath each other
+
+            PageCart.AddtoCart(driver,session);
+
         }
 
         [Test]
--- PageCart.cs
+++ PageCart.cs
@@ -1,6 +1,18 @@
-﻿namespace Capstone
+﻿using OpenQA.Selenium;
+using SikuliSharp;
+
+namespace Capstone
 {
     internal class PageCart
     {
+        public static void AddtoCart(IWebDriver driver, ISikuliSession session) 
+            
+        {
+            string path = "C:\\Training\\CapstoneProject\\Images";     // Click on the product labled Create Your Own Jewelry image (Use Sikuli).  Product Page will be displayed
+            IPattern iPattern = Patterns.FromFile(path);
+            session.Wait(iPattern);
+            session.Click(iPattern);
+
+        }
     }
 }
